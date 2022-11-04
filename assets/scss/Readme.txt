88d72bad50cd209463bb090ca556d95ce59ec7b8
vishalv222 <117445883+vishalv222@users.noreply.github.com>
2022-11-04 16:18:50 +0530

    first commit

---
+++ assets/scss/Readme.txt
@@ -0,0 +1,1 @@
+The .scss (Sass) files are only avilable in the pro version. 
---
+++ forms/book-a-table.php
@@ -0,0 +1,30 @@
+<?php
+ 
+  $receiving_email_address = 'vishalvishwakarma1509@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $book_a_table = new PHP_Email_Form;
+  $book_a_table->ajax = true;
+  
+  $book_a_table->to = $receiving_email_address;
+  $book_a_table->from_name = $_POST['name'];
+  $book_a_table->from_email = $_POST['email'];
+  $book_a_table->subject = "New table booking request from the website";
+
+  
+
+  $book_a_table->add_message( $_POST['name'], 'Name');
+  $book_a_table->add_message( $_POST['email'], 'Email');
+  $book_a_table->add_message( $_POST['phone'], 'Phone', 4);
+  $book_a_table->add_message( $_POST['date'], 'Date', 4);
+  $book_a_table->add_message( $_POST['time'], 'Time', 4);
+  $book_a_table->add_message( $_POST['people'], '# of people', 1);
+  $book_a_table->add_message( $_POST['message'], 'Message');
+
+  echo $book_a_table->send();
+?>
